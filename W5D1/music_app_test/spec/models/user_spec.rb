7e716a54f4a84cb4b35b721ff10dda7f2b267e77
Jonathan Abantao <jonabantao@gmail.com>
2018-01-07 20:15:48 -0800

    Use Factory Bot and Faker for User specs

--- W5D1/music_app_test/spec/models/user_spec.rb
+++ W5D1/music_app_test/spec/models/user_spec.rb
@@ -13,6 +13,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject(:user) { FactoryBot.build(:user) }
+
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password_digest) }
@@ -20,38 +22,32 @@ RSpec.describe User, type: :model do
   end
 
   describe '#is_password?' do
-    test_user = User.new
-    test_user.password = "tester"
-
     it 'verfies correct password' do
-      expect(test_user.is_password?("tester")).to be true
+      expect(user.is_password?(user.password)).to be true
     end
 
     it 'rejects incorrect password' do
-      expect(test_user.is_password?("notmypassword")).to be false
+      expect(user.is_password?("smashthestate")).to be false
     end
   end
 
   describe '#reset_session_token!' do
-    test_cookie = User.new
-    cookie1 = test_cookie.reset_session_token!
-    cookie2 = test_cookie.reset_session_token!
-
     it 'should change session_token when calling reset_session_token!' do
+      cookie1 = user.session_token
+      cookie2 = user.reset_session_token!
       expect(cookie1).to_not eq(cookie2)
     end
   end
 
-  # Uses factory which is supposedly covered "tomorrow"
-  # describe ".find_by_credentials" do
-  #   before { user.save! }
-  #
-  #   it "returns user given good credentials" do
-  #     expect(User.find_by_credentials("jonathan@fakesite.com", "good_password")).to eq(user)
-  #   end
-  #
-  #   it "returns nil given bad credentials" do
-  #     expect(User.find_by_credentials("jonathan@fakesite.com", "bad_password")).to eq(nil)
-  #   end
-  # end
+  describe "::find_by_credentials" do
+    before { user.save! }
+
+    it "returns user given good credentials" do
+      expect(User.find_by_credentials(user.email, user.password)).to eq(user)
+    end
+
+    it "returns nil given bad credentials" do
+      expect(User.find_by_credentials(user.email, "smashthestate")).to eq(nil)
+    end
+  end
 end
